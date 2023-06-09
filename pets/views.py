2de19eda452a986560d7469ff862504615d591e8
isabelpaiva <isxpaiva@icloud.com>
2023-06-09 16:29:54 -0300

    feat: Implements POST endpoint in PetViews class

---
+++ pets/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from .serializers import PetsSerializer
+from groups.models import Group
+from traits.models import Trait
+from pets.models import Pet
+
+class PetViews(APIView, PageNumberPagination):
+    def post(self, request):
+        serializer = PetsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        group = serializer.validated_data.pop("group")
+        traits = serializer.validated_data.pop("traits")
+
+        group_objeto = Group.objects.filter(scientific_name__iexact=group["scientific_name"]).first()
+        if not group_objeto:
+            new_group = Group.objects.create(**group)
+            pet = Pet.objects.create(**serializer.validated_data, group=new_group)
+        else:
+            pet = Pet.objects.create(**serializer.validated_data, group=group_objeto)
+        
+
+        for trait in traits:
+            trait_object = Trait.objects.filter(name__iexact=trait["name"]).first()
+            if not trait_object:
+                trait_object = Trait.objects.create(**trait)
+            pet.traits.add(trait_object)
+        
+        serializer = PetsSerializer(pet)
+        return Response(serializer.data, status=201)
+
+        
+
+        
+
